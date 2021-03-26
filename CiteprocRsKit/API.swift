3b3a56c03a8efc35902060700cfb346fb581cdf1
Cormac Relf <web@cormacrelf.net>
2021-03-26 18:46:39 +1100

    api: Add more features and more tests
    - It actually frees itself now :)
    - Can set the output format
    - Has a one_ref_citation() function that takes a (reference: Any) -> String
    - More streamlined init
    - Most things it has are now tested
    - I got a lot better at writing swift in the past day
    
    The citeproc-ffi library needs quite a lot of work especially for error returning.

--- CiteprocRsKit/API.swift
+++ CiteprocRsKit/API.swift
@@ -9,9 +9,24 @@ import Foundation
 import CoreText
 import CiteprocRs
 
+// Without NS_ENUM support in cbindgen, it's pretty hard to use the citeproc_rs_output_format enum directly from Swift.
+// It's typedef'd to a uint8_t etc. So just make sure these numbers match the ones in the header.
+public enum OutputFormat: UInt8 {
+    case html = 0
+    case rtf = 1
+    case plain = 2
+}
+
 public struct InitOptions {
-    public let style: String
-    public let locale_callback: (String) -> String?
+    public init(style: String, locale_callback: @escaping (String) -> String?, output_format: OutputFormat = .html) {
+        self.style = style
+        self.locale_callback = locale_callback
+        self.output_format = output_format
+    }
+    
+    public var style: String
+    public var locale_callback: (String) -> String?
+    public var output_format: OutputFormat = .html
 }
 
 struct FetchContext {
@@ -19,8 +34,11 @@ struct FetchContext {
 }
 
 func locale_fetch_callback(ctx_raw: UnsafeMutableRawPointer?, slot: OpaquePointer?, lang_cstr: UnsafePointer<Int8>?) {
-    print("hi from doit")
-    if let locale = ctx_raw?.assumingMemoryBound(to: FetchContext.self).pointee.locale_callback("NONONONON") {
+    guard let lang_ptr = lang_cstr else {
+        return
+    }
+    let lang = String.init(cString: lang_ptr)
+    if let locale = ctx_raw?.assumingMemoryBound(to: FetchContext.self).pointee.locale_callback(lang) {
         let data: Data? = locale.data(using: .utf8, allowLossyConversion: false)
         data?.withUnsafeBytes({ bytes in
             // why is unsafebufferpointer.count a signed integer????????????????
@@ -29,40 +47,74 @@ func locale_fetch_callback(ctx_raw: UnsafeMutableRawPointer?, slot: OpaquePointe
             citeproc_rs_write_locale_slot(slot, baseAddress, count)
         })
     }
-    
-
 }
 
-public struct CiteprocRsDriver {
+public enum BindingsError: Error {
+    case invalidUtf8
+    case internalError(String)
+}
+
+public class CiteprocRsDriver {
+    internal init(raw: OpaquePointer, fetch_ctx: UnsafePointer<FetchContext>) {
+        self.raw = raw
+        self.fetch_ctx = fetch_ctx
+    }
+    
     private let raw: OpaquePointer
     private let fetch_ctx: UnsafePointer<FetchContext>
-    private static func from_raw(raw: OpaquePointer?, fetch_ctx: UnsafePointer<FetchContext>) -> Self? {
-        if let raw = raw {
-            return Self.init(raw: raw, fetch_ctx: fetch_ctx)
-        } else {
-            return nil
-        }
-    }
-    public static func new(options: InitOptions) -> CiteprocRsDriver? {
+    
+    public init(_ options: InitOptions) throws {
         let ctx: UnsafeMutablePointer<FetchContext> = UnsafeMutablePointer.allocate(capacity: 1)
         ctx.initialize(to: FetchContext(locale_callback: options.locale_callback))
         let ctx_raw: UnsafeMutableRawPointer! = UnsafeMutableRawPointer(ctx)
         
-        let style = options.style;
-        let data: Data? = style.data(using: .utf8, allowLossyConversion: false)
-        let raw: OpaquePointer? =  data?.withUnsafeBytes({ bytes in
-            // why is unsafebufferpointer.count a signed integer????????????????
-            let count: UInt = UInt(bytes.count)
-            let baseAddress: UnsafePointer<Int8> = bytes.baseAddress!.assumingMemoryBound(to: Int8.self)
+        guard let data = options.style.data(using: .utf8, allowLossyConversion: false) else {
+            throw BindingsError.invalidUtf8
+        }
+        let maybe_raw: OpaquePointer? =  with_data_as_char_ptr(data, { style, style_len in
             let options = citeproc_rs_init_options(
-                style: baseAddress,
-                style_len: count,
+                style: style,
+                style_len: style_len,
                 locale_fetch_context: ctx_raw,
                 locale_fetch_callback: locale_fetch_callback,
-                format: CiteprocRs.citeproc_rs_output_format(0) // HTML
+                format: CiteprocRs.citeproc_rs_output_format(options.output_format.rawValue)
             )
             return CiteprocRs.citeproc_rs_new(options)
         })
-        return CiteprocRsDriver.from_raw(raw: raw, fetch_ctx: ctx)
+        guard let raw = maybe_raw  else {
+            throw BindingsError.internalError("Null driver returned from citeproc_rs")
+        }
+        self.raw = raw
+        self.fetch_ctx = UnsafePointer(ctx) // no longer mutable outside callback, but also don't read it
     }
+    
+    public func one_ref_citation(_ reference: Any) throws -> String? {
+        let ref_json: Data = try JSONSerialization.data(withJSONObject: reference)
+        guard let ptr = with_data_as_char_ptr(ref_json, { buf, buf_len in
+            return CiteprocRs.citeproc_rs_format_one(self.raw, buf, buf_len)
+        }) else {
+            return nil
+        }
+        return string_from_rust_cstring(ptr)
+    }
+    
+    deinit {
+        self.fetch_ctx.deallocate()
+        CiteprocRs.citeproc_rs_free(self.raw)
+    }
+}
+
+internal func with_data_as_char_ptr<T>(_ data: Data, _ f: (UnsafePointer<Int8>, UInt) -> T) -> T {
+    return data.withUnsafeBytes({ bytes in
+        let baseAddress: UnsafePointer<Int8> = bytes.baseAddress!.assumingMemoryBound(to: Int8.self)
+        // why is unsafebufferpointer.count a signed integer????????????????
+        let count: UInt = UInt(bytes.count)
+        return f(baseAddress, count)
+    })
+}
+
+internal func string_from_rust_cstring(_ ptr: UnsafeMutablePointer<CChar>) -> String {
+    let s = String.init(cString: ptr)
+    CiteprocRs.citeproc_rs_string_free(ptr)
+    return s
 }
--- CiteprocRsKitTests/CiteprocRsKitTests.swift
+++ CiteprocRsKitTests/CiteprocRsKitTests.swift
@@ -8,6 +8,61 @@
 import XCTest
 @testable import CiteprocRsKit
 
+let EN_GB = mklocale(lang: "en-GB")
+let style = mkstyle(default_locale: "en-GB");
+func mklocale(lang: String, terms: String = "") -> String {
+    return """
+    <locale
+        version="1.0"
+        xml:lang="\(lang)">
+        <info>
+            <updated>2015-10-10T23:31:02+00:00</updated>
+        </info>
+        <terms>\(terms)</terms>
+    </locale>
+    """;
+}
+func mkcitation(_ layout_inner: String = "") -> String {
+    return """
+    <citation><layout>\(layout_inner)</layout></citation>
+    """
+}
+func mkstyle(
+    default_locale: String = "en-GB",
+    style_class: String = "note",
+    macros: String = "",
+    citation: String = "<citation><layout><text variable=\"title\" /></layout></citation>",
+    bibliography: String = ""
+) -> String {
+    return """
+    <style xmlns="http://purl.org/net/xbiblio/csl"
+    class="\(style_class)"
+    version="1.0"
+    default-locale="\(default_locale)">
+        <info>
+            <id>id</id>
+            <title>title</title>
+            <updated>2015-10-10T23:31:02+00:00</updated>
+        </info>
+        \(macros)
+        \(citation)
+        \(bibliography)
+    </style>
+    """;
+}
+
+let locale_callback: (String) -> String? = { lang in
+    if lang == "en-GB" {
+        // print("returning en-GB locale")
+        return EN_GB
+    } else {
+        print("I don't know that locale " + lang)
+        return nil
+    }
+}
+
+let OPTIONS = InitOptions(style: style, locale_callback: locale_callback)
+
 class CiteprocRsKitTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -18,29 +73,67 @@ class CiteprocRsKitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        let EN_US = "<locale version=\"1.0\" xml:lang=\"en-US\">\n<info> <updated>2015-10-10T23:31:02+00:00</updated> </info><terms> </terms></locale>"
-        let style = "<style xmlns=\"http://purl.org/net/xbiblio/csl\" class=\"note\" version=\"1.0\" default-locale=\"en-GB\"><info><id>id</id><title>title</title><updated>2015-10-10T23:31:02+00:00</updated></info> <citation><layout><text variable=\"title\" /></layout></citation></style>";
-        
-        let locale_callback: (String) -> String? = { lang in
-            if lang == "en-US" {
-                print("returning en-US locale")
-                return EN_US
-            } else {
-                print("I don't know that locale " + lang)
-                return nil
-            }
-        }
-        let driver = CiteprocRsDriver.new(options: InitOptions(style: style, locale_callback: locale_callback))
-        
-        XCTAssert(driver != nil)
+    func testInit() throws {
+        // Let's force it to deinit just so we know
+        var d: CiteprocRsDriver? = try CiteprocRsDriver(OPTIONS)
+        XCTAssert(d != nil)
+        d = nil
     }
-
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+    
+    func testLocaleFetch() throws {
+        let style = mkstyle(default_locale: "de-AT")
+        var langs: [String] = []
+        let _ = try CiteprocRsDriver(InitOptions(style: style, locale_callback: { lang in
+            langs.append(lang)
+            return nil
+        }))
+        langs.sort()
+        XCTAssertEqual(langs, ["de-AT", "de-DE"])
+    }
+    
+    func testOne() throws {
+        let driver = try CiteprocRsDriver(OPTIONS)
+        let empty = try driver.one_ref_citation(["id": "refid", "type": "book"])
+        XCTAssertEqual(empty, "[CSL STYLE ERROR: reference with no printed form.]")
+        let title = try driver.one_ref_citation(["id": "refid", "type": "book", "title": "the title"])
+        XCTAssertEqual(title, "the title")
+    }
+    
+    func testUsesLocale() throws {
+        let style = mkstyle(default_locale: "en-GB", citation: mkcitation("""
+            <text term="forthcoming" />
+        """))
+        let chaps = "one moment chaps"
+        let driver = try CiteprocRsDriver(InitOptions(style: style, locale_callback: { lang in
+            return mklocale(lang: "en-GB", terms: """
+                <term name="forthcoming">\(chaps)</term>
+            """)
+        }))
+        let output = try driver.one_ref_citation(["id": "refid", "type": "book"])
+        XCTAssertEqual(output, "One moment chaps")
+    }
+    
+    func testAppliesOutputFormat() throws {
+        let style = mkstyle(default_locale: "en-GB", citation: mkcitation("""
+            <text font-style="italic" value="text" />
+        """))
+        let tstdriver: (OutputFormat, String) throws -> () = { fmt, res in
+            let options = InitOptions(style: style, locale_callback: { mklocale(lang: $0) }, output_format: fmt)
+            let d = try CiteprocRsDriver(options)
+            let out = try d.one_ref_citation(["id": "refid", "type": "book"])
+            XCTAssertEqual(out, res)
+        }
+        let thrower = {
+            throw BindingsError.internalError("ok")
+        }
+        do {
+            try thrower()
+        } catch let e as BindingsError {
+            print(e)
+        }
+        try tstdriver(.html, "<i>text</i>")
+        try tstdriver(.rtf, "{\\i text}")
+        try tstdriver(.plain, "text")
+    }
 
 }
