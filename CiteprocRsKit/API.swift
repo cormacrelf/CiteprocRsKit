7c918b67ca300706f3480883b1b57b2d6c2d8b72
Cormac Relf <web@cormacrelf.net>
2021-07-09 14:31:12 +1000

    Work with new cbindgen output for swift

--- CiteprocRsKit/API.swift
+++ CiteprocRsKit/API.swift
@@ -9,13 +9,7 @@ import Foundation
 import CoreText
 import CiteprocRs
 
-// Without NS_ENUM support in cbindgen, it's pretty hard to use the citeproc_rs_output_format enum directly from Swift.
-// It's typedef'd to a uint8_t etc. So just make sure these numbers match the ones in the header.
-public enum OutputFormat: UInt8 {
-    case html = 0
-    case rtf = 1
-    case plain = 2
-}
+public typealias OutputFormat = CiteprocRs.CiteprocRsOutputFormat
 
 public struct InitOptions {
     public init(style: String, locale_callback: @escaping (String) -> String?, output_format: OutputFormat = .html) {
@@ -44,7 +38,7 @@ func locale_fetch_callback(ctx_raw: UnsafeMutableRawPointer?, slot: OpaquePointe
             // why is unsafebufferpointer.count a signed integer????????????????
             let count = UInt(bytes.count)
             let baseAddress = bytes.baseAddress?.assumingMemoryBound(to: Int8.self)
-            citeproc_rs_write_locale_slot(slot, baseAddress, count)
+            CiteprocRs.citeproc_rs_write_locale_slot(slot: slot, locale_xml: baseAddress, locale_xml_len: count)
         })
     }
 }
@@ -72,16 +66,16 @@ public class CiteprocRsDriver {
             throw BindingsError.invalidUtf8
         }
         let maybe_raw: OpaquePointer? =  with_data_as_char_ptr(data, { style, style_len in
-            let options = citeproc_rs_init_options(
+            let options = CiteprocRsInitOptions(
                 style: style,
                 style_len: style_len,
                 locale_fetch_context: ctx_raw,
                 locale_fetch_callback: locale_fetch_callback,
-                format: CiteprocRs.citeproc_rs_output_format(options.output_format.rawValue)
+                format: options.output_format
             )
-            return CiteprocRs.citeproc_rs_new(options)
+            return citeproc_rs_processor_new(init: options)
         })
-        guard let raw = maybe_raw  else {
+        guard let raw = maybe_raw else {
             throw BindingsError.internalError("Null driver returned from citeproc_rs")
         }
         self.raw = raw
@@ -91,7 +85,7 @@ public class CiteprocRsDriver {
     public func one_ref_citation(_ reference: Any) throws -> String? {
         let ref_json: Data = try JSONSerialization.data(withJSONObject: reference)
         guard let ptr = with_data_as_char_ptr(ref_json, { buf, buf_len in
-            return CiteprocRs.citeproc_rs_format_one(self.raw, buf, buf_len)
+            return citeproc_rs_processor_format_one(processor: self.raw, ref_bytes: buf, ref_bytes_len: buf_len)
         }) else {
             return nil
         }
@@ -100,7 +94,7 @@ public class CiteprocRsDriver {
     
     deinit {
         self.fetch_ctx.deallocate()
-        CiteprocRs.citeproc_rs_free(self.raw)
+        citeproc_rs_processor_free(processor: self.raw)
     }
 }
 
@@ -115,6 +109,6 @@ internal func with_data_as_char_ptr<T>(_ data: Data, _ f: (UnsafePointer<Int8>, 
 
 internal func string_from_rust_cstring(_ ptr: UnsafeMutablePointer<CChar>) -> String {
     let s = String.init(cString: ptr)
-    CiteprocRs.citeproc_rs_string_free(ptr)
+    CiteprocRs.citeproc_rs_string_free(ptr: ptr)
     return s
 }
