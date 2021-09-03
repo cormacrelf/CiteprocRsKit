c1ae4a6269bdf732efb81966a37aaa80fc0429ec
Cormac Relf <web@cormacrelf.net>
2021-09-04 00:35:11 +1000

    v1 dates, more JSON tests

--- Sources/CiteprocRsKit/CslJson.swift
+++ Sources/CiteprocRsKit/CslJson.swift
@@ -1,14 +1,15 @@
 //
 //  CslJson.swift
-//  
+//
 //
 //  Created by Cormac Relf on 31/8/21.
 //
 
 import Foundation
 
-// If anyone needs this to be Decodable (unlikely), there's https://paul-samuels.com/blog/2019/01/02/swift-heterogeneous-codable-array/
+// If anyone needs this to be Decodable, there's https://paul-samuels.com/blog/2019/01/02/swift-heterogeneous-codable-array/
 // TODO: make this Equatable and Hashable
+
 public struct CslReference: Encodable {
     public var id: String
     public var type: String
@@ -19,25 +20,30 @@ public struct CslReference: Encodable {
         self.type = type
         self.variables = variables
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(self.id, forKey: CodingKeys.id)
         try container.encode(self.type, forKey: CodingKeys.type)
         for (key, variable) in self.variables {
-            try container.encode(variable, forKey: CodingKeys.init(stringValue: key)!)
+            if let key = CodingKeys.init(stringValue: key) {
+                guard case .variable = key else {
+                    continue
+                }
+                try container.encode(variable, forKey: key)
+            }
         }
     }
-    
+
     enum CodingKeys: CodingKey {
         var intValue: Int? {
             return nil
         }
-        
+
         init?(intValue: Int) {
             return nil
         }
-        
+
         var stringValue: String {
             switch self {
             case .id: return "id"
@@ -45,15 +51,21 @@ public struct CslReference: Encodable {
             case .variable(let s): return s
             }
         }
-        
+
         init?(stringValue: String) {
             switch stringValue {
-            case "id": self = .id; break
-            case "type": self = .type; break
-            default: self = .variable(stringValue); break
+            case "id":
+                self = .id
+                break
+            case "type":
+                self = .type
+                break
+            default:
+                self = .variable(stringValue)
+                break
             }
         }
-        
+
         case id
         case type
         case variable(String)
@@ -61,14 +73,14 @@ public struct CslReference: Encodable {
 }
 
 public enum CslVariable: Encodable {
-    
+
     case string(String)
     case number(NumString)
     case names([CslName])
     case date(CslDate)
     /// CSL 1.1
     case title(CslTitle)
-    
+
     public func encode(to encoder: Encoder) throws {
         switch self {
         case .string(let s): try s.encode(to: encoder)
@@ -92,29 +104,106 @@ public enum NumString: Encodable {
 }
 
 public struct CslName: Encodable {
-    public var family: String?
-    public var given: String?
-    
-    public init(family: String? = nil, given: String? = nil) {
+
+    public init(
+        family: String? = nil, given: String? = nil,
+        droppingParticle: String? = nil, nonDroppingParticle: String? = nil
+    ) {
         self.family = family
         self.given = given
+        self.droppingParticle = droppingParticle
+        self.nonDroppingParticle = nonDroppingParticle
+    }
+
+    public init(
+        literal: String? = nil
+    ) {
+        self.literal = literal
+    }
+
+    public var family: String? = nil
+    public var given: String? = nil
+    public var literal: String? = nil
+    public var droppingParticle: String? = nil
+    public var nonDroppingParticle: String? = nil
+
+    enum CodingKeys: String, CodingKey {
+        case family, given, literal
+        case droppingParticle = "dropping-particle"
+        case nonDroppingParticle = "non-dropping-particle"
     }
 }
 
 public enum CslDate: Encodable {
-    case edtf(String)
-    case legacy(Legacy)
-    
-    public struct Legacy: Encodable {
-        public var dateParts: [[Int]]?
-        public var circa: Bool?
-        // TODO: more fields, also circa is called approximate now?
+
+    // enable when supported
+    // case edtf(String)
+    case v1(V1)
+
+    public struct V1: Encodable {
+
+        public var dateParts: [[Int]]? = nil
+        public var raw: String? = nil
+        public var literal: String? = nil
+        // enable when supported
+        // public var edtf: String?
+
+        public var circa: Bool? = nil
+        public var season: NumString? = nil
+
+        public init(
+            dateParts: [[Int]],
+            circa: Bool? = nil, season: NumString? = nil
+        ) {
+            self.dateParts = dateParts
+            self.circa = circa
+            self.season = season
+        }
+
+        public init(
+            raw: String,
+            circa: Bool? = nil, season: NumString? = nil
+        ) {
+            self.raw = raw
+            self.circa = circa
+            self.season = season
+        }
+
+        public init(
+            literal: String,
+            circa: Bool? = nil, season: NumString? = nil
+        ) {
+            self.literal = literal
+            self.circa = circa
+            self.season = season
+        }
+
+        // A Foundation.Date is pretty annoying for us, because it has no timezone information
+        // and hence we cannot know which zone to render it in. It always includes a time, and this
+        // will end up with off-by-one-day errors.
+
+        // So this kind of method is no good.
+        // /// Make sure you're creating date objects using GMT. Otherwise the rendered date will be wrong.
+        // @available(macOS 10.12, *)
+        // public init(rawFrom date: Date) {
+        //     let fmt = ISO8601DateFormatter()
+        //     fmt.timeZone = .init(secondsFromGMT: 0)
+        //     fmt.formatOptions.remove(.withFullTime)
+        //     let iso = fmt.string(from: date)
+        //     self = V1(raw: iso)
+        // }
+
+        enum CodingKeys: String, CodingKey {
+            case dateParts = "date-parts"
+            case circa, season, literal, raw
+            // case edtf
+        }
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         switch self {
-        case .edtf(let edtf): return try edtf.encode(to: encoder)
-        case .legacy(let legacy): return try legacy.encode(to: encoder)
+        // case .edtf(let edtf): return try edtf.encode(to: encoder)
+        case .v1(let v1): return try v1.encode(to: encoder)
         }
     }
 }
@@ -122,9 +211,11 @@ public enum CslDate: Encodable {
 public enum CslTitle: Encodable {
     case string(String)
     case object(TitleObject)
-    
+
     public struct TitleObject: Encodable {
-        public init(full: String? = nil, main: String? = nil, sub: [String]? = nil, short: String? = nil) {
+        public init(
+            full: String? = nil, main: String? = nil, sub: [String]? = nil, short: String? = nil
+        ) {
             self.full = full
             self.main = main
             self.sub = sub
--- Sources/CiteprocRsKitTests/CslJsonTests.swift
+++ Sources/CiteprocRsKitTests/CslJsonTests.swift
@@ -5,26 +5,30 @@
 //  Created by Cormac Relf on 31/8/21.
 //
 
+import CiteprocRsKit
 import Foundation
 import XCTest
-import CiteprocRsKit
 
-@available(macOS 10.13, *) // .sortedKeys
+@available(macOS 10.15, *)  // .sortedKeys, .withoutEscapingSlashes
 final class CslJsonTests: XCTestCase {
     var encoder: JSONEncoder = JSONEncoder()
     override func setUp() {
         // for reliable tests
-        encoder.outputFormatting = .prettyPrinted.union(.sortedKeys)
+        encoder.outputFormatting = .prettyPrinted.union(.sortedKeys).union(.withoutEscapingSlashes)
     }
+
     func testStringVariable() throws {
-        let variable: CslVariable = .string("hello");
+        let variable: CslVariable = .string("hello")
         let result = try encoder.encode(variable)
         XCTAssertEqual(String(data: result, encoding: .utf8), "\"hello\"")
     }
+
     func testNameVariable() throws {
-        let variable: CslVariable = .names([CslName(family: "Smith", given: "John")]);
+        let variable: CslVariable = .names([CslName(family: "Smith", given: "John")])
         let result = try encoder.encode(variable)
-        XCTAssertEqual(String(data: result, encoding: .utf8)!, """
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
             [
               {
                 "family" : "Smith",
@@ -33,26 +37,60 @@ final class CslJsonTests: XCTestCase {
             ]
             """)
     }
-    
+
+    func testTitleString() throws {
+        let variable: CslVariable = .title(.string("Main Title: Subtitle"))
+        let result = try encoder.encode(variable)
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
+            "Main Title: Subtitle"
+            """)
+    }
+
     func testTitles() throws {
         let variable: CslVariable = .title(.object(.init(full: "Main Title: Subtitle")))
         let result = try encoder.encode(variable)
-        XCTAssertEqual(String(data: result, encoding: .utf8)!, """
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
             {
               "full" : "Main Title: Subtitle"
             }
             """)
     }
 
+    func testDateParts() throws {
+        let variable: CslVariable = .date(.v1(.init(dateParts: [[2021, 2, 1]])))
+        let result = try encoder.encode(variable)
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
+            {
+              "date-parts" : [
+                [
+                  2021,
+                  2,
+                  1
+                ]
+              ]
+            }
+            """)
+    }
+
     func testReference() throws {
-        let reference: CslReference = CslReference(id: "id", type: "book", variables: [
-            "author": .names([CslName(family: "Smith", given: "John")]),
-            "issued": .date(.edtf("2021")),
-            "version": .number(.int(2)),
-            "title": .string("Title"),
-        ])
+        let reference: CslReference = CslReference(
+            id: "id", type: "book",
+            variables: [
+                "author": .names([CslName(family: "Smith", given: "John")]),
+                "issued": .date(.v1(.init(raw: "2021"))),
+                "version": .number(.int(2)),
+                "title": .string("Title"),
+            ])
         let result = try encoder.encode(reference)
-        XCTAssertEqual(String(data: result, encoding: .utf8)!, """
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
             {
               "author" : [
                 {
@@ -61,13 +99,32 @@ final class CslJsonTests: XCTestCase {
                 }
               ],
               "id" : "id",
-              "issued" : "2021",
+              "issued" : {
+                "raw" : "2021"
+              },
               "title" : "Title",
               "type" : "book",
               "version" : 2
             }
             """)
     }
-    
-    
+
+    func testBadVarName() throws {
+        let reference: CslReference = CslReference(
+            id: "id", type: "book",
+            variables: [
+                "id": .string("String"),
+                "type": .string("String"),
+            ])
+        let result = try encoder.encode(reference)
+        XCTAssertEqual(
+            String(data: result, encoding: .utf8)!,
+            """
+            {
+              "id" : "id",
+              "type" : "book"
+            }
+            """)
+    }
+
 }
